f01019e1745a3464fb60f456413216fd800d5581
KeithSRoberts <robertsskeith@gmail.com>
2019-12-20 12:44:01 -0800

    Return content instead of response headers on success

--- uw_spotseeker/dao.py
+++ uw_spotseeker/dao.py
@@ -34,4 +34,4 @@ class Spotseeker_LiveDAO(LiveDAO):
                                        method=method,
                                        body=body,
                                        headers=headers)
-        return resp
+        return content
