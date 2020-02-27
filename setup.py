30ba2c732269841756322b98e967c0c2d4f766c2
KeithSRoberts <robertsskeith@gmail.com>
2020-02-27 10:10:49 -0800

    Pinned UW-Restclients-Core for python 2 compat, removed unused imports in dao

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author="UW-IT AXDD",
     author_email="aca-it@uw.edu",
     include_package_data=True,
-    install_requires=['UW-RestClients-Core',
+    install_requires=['UW-RestClients-Core<1.0,>=0.9',
                       'python-dateutil',
                       'Pillow',
                       'mock',
