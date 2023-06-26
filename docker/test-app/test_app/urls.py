e7a2c0c6f0626c3cd3c0a2745ac5d7b353e220df
David-Strupinski <dstrupin@uw.edu>
2023-06-26 09:34:03 -0700

    remove unused ItemImageView

--- docker/test-app/test_app/urls.py
+++ docker/test-app/test_app/urls.py
@@ -1,19 +1,15 @@
-# Copyright 2022 UW-IT, University of Washington
+# Copyright 2023 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from .views import SearchSpotsView, SpotView, ItemImageView, SpotImageView, \
-     AllSpotsView, BuildingsView
+from .views import SearchSpotsView, SpotView, SpotImageView, AllSpotsView, \
+     BuildingsView
 
 urlpatterns = [
     path("spot/<int:spot_id>/image/<int:img_id>",
          csrf_exempt(SpotImageView.as_view())),
     path("spot/<int:spot_id>/image", csrf_exempt(SpotImageView.as_view())),
-
-    path("item/<int:item_id>/image/<int:img_id>",
-         csrf_exempt(ItemImageView.as_view())),
-    path("item/<int:item_id>/image", csrf_exempt(ItemImageView.as_view())),
 
     path("spot/<int:spot_id>", csrf_exempt(SpotView.as_view())),
     path("spot", csrf_exempt(SpotView.as_view())),
